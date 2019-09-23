f826bd69462bbb6d549056e36e678514fd02ff91
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2019-09-23 15:42:36 +0100

    date ranges

--- lib/swab.rb
+++ lib/swab.rb
@@ -6,10 +6,17 @@ module Swab
   class Error < StandardError; end
   
   
-  def self.swab(url, user, pass)
+  def self.swab(url, user, pass, days, spinner)
     parser = Parser.new(url)
+    
+    spinner.update(title: 'Logging in' ) if spinner
     parser.login(user, pass )
-    data = parser.parse
+    
+    spinner.update(title: 'Setting date range' ) if spinner
+    parser.set_date_range(days)
+    
+    spinner.update(title: 'Scraping' ) if spinner
+    data = parser.parse { |page| spinner.update(title: "Scaping page #{page+1}") }
 #     puts data
   end
 end
--- lib/swab/parser.rb
+++ lib/swab/parser.rb
@@ -1,9 +1,16 @@
+require "logger"
+
 module Swab
 
   class Parser
+    attr_accessor :page
+    
     def initialize(url)
+    
+      @log = Logger.new STDOUT
+      @log.level = Logger::DEBUG
       @base_url = url
-      @mech = Mechanize.new
+      @mech = Mechanize.new # { |a| a.log = @log }
     end
     
     def login(username, password)
@@ -14,6 +21,21 @@ module Swab
       end.click_button
     end
     
+    def set_date_range(days)
+      start_date = Date.today
+      end_date = start_date + days - 1
+      
+      form = @page.form_with(name: 'aspnetForm')
+      form.add_field!('ctl00$ToolkitScriptManager1', 'ctl00$content$BookingStatusUpdatePanel|ctl00$content$BookingStatus1$cmdSubmitPrint')
+      form.add_field!('ctl00$content$BookingStatus1$cmdSubmitPrint', 'Filter')
+      form.add_field!('__EVENTARGUMENT', '')
+      form.add_field!('__EVENTTARGET', '')
+      form['ctl00$content$BookingStatus1$mListDateOptions'] = 'DateRange'
+      form['ctl00$content$BookingStatus1$StartDate'] = start_date.strftime '%d-%b-%y'
+      form['ctl00$content$BookingStatus1$EndDate'] = end_date.strftime '%d-%b-%y'
+      @page = form.submit
+    end
+    
     def parse(data = [], count = 0)
       table = @page.search('table#ctl00_content_BookingStatus1_dynamicTable')
       new_data = table.search('tr')[1..-1].map do |tr|
@@ -21,13 +43,15 @@ module Swab
       end
       
       data = data + new_data
+      
+      yield(count) if block_given?
             
       form = @page.form_with(name: 'aspnetForm')
       form['__EVENTTARGET'] = 'ctl00$content$BookingStatus1'
       form['__EVENTARGUMENT'] = "goToPage_#{count + 2}"
       if !new_data.empty?
         @page = form.submit
-        data = parse(data, count + 1)
+        data = parse(data, count + 1, &Proc.new)
       end
       data
     end
